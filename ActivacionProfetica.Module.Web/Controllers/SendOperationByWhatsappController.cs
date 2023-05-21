d307b2ef20cf591c952ee20ba82768d1b93c85c8
Eduardo Barja <kevinbarja@gmail.com>
2023-05-21 13:49:09 -0400

    Add params internal id

--- ActivacionProfetica.Module.Web/Controllers/SendOperationByWhatsappController.cs
+++ ActivacionProfetica.Module.Web/Controllers/SendOperationByWhatsappController.cs
@@ -6,6 +6,7 @@ using DevExpress.ExpressApp.ReportsV2;
 using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl;
 using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.Parameters;
 using DevExpress.XtraReports.UI;
 using System;
 using System.IO;
@@ -56,6 +57,12 @@ namespace ActivacionProfetica.Module.Web.Controllers
 
             XtraReport reporte = ReportDataProvider.ReportsStorage.GetReportContainerByHandle(
                 ReportDataProvider.ReportsStorage.GetReportContainerHandle(reportData)).Report;
+            reporte.FilterString = "[InternalId] = ?paramInternalId";
+            reporte.Parameters.Add(new Parameter
+            {
+                Name = "paramInternalId",
+                Value = operation.InternalId
+            });
             moduloReportes.ReportsDataSourceHelper.SetupBeforePrint(reporte, null, null, false, null, false);
 
             using (MemoryStream flujo = new MemoryStream())
@@ -66,7 +73,7 @@ namespace ActivacionProfetica.Module.Web.Controllers
                 flujo.Seek(0, SeekOrigin.Begin);
                 byte[] contenidoReporte = flujo.ToArray();
                 string base64 = Convert.ToBase64String(contenidoReporte);
-                string json = "{\"from_number\": \"" + number_whatsapp + "\", \"message\": \"Operacion Exitosa "+operation.InternalId.ToString()+"\", \"documento\": \"" + base64 + "\"}";
+                string json = "{\"from_number\": \"" + number_whatsapp + "\", \"message\": \"Operacion Exitosa " + operation.InternalId.ToString() + "\", \"documento\": \"" + base64 + "\"}";
                 StringContent httpContent = new StringContent(json, Encoding.UTF8, "application/json");
                 httpClient.PostAsync("http://envio-whatsapp.herokuapp.com/webhook", httpContent);
             }
