525e4bcac53da9df7922d85af45926d381af2503
Eduardo Barja <kevinbarja@gmail.com>
2023-05-25 16:49:51 -0400

    Add version controller

---
+++ ActivacionProfetica.Module.Web/Controllers/VersionController.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
+using System.Web.Configuration;
+
+namespace ActivacionProfetica.Module.Web.Controllers
+{
+    public class MyController : WindowController
+    {
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            AboutInfo.Instance.Version = GetEnviroment();
+        }
+
+        private string GetEnviroment()
+        {
+            string enviroment = WebConfigurationManager.AppSettings["Enviroment"];
+            string defaultEnviroment = "Ambiente no definido";
+            if (enviroment != null)
+            {
+                return enviroment;
+            }
+            return defaultEnviroment;
+        }
+    }
+}
