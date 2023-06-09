aaa405347f6ba741143234314a563b82e273016e
Eduardo Barja <kevinbarja@gmail.com>
2023-06-09 12:22:15 -0400

    Fix ui on customer loockup

---
+++ ActivacionProfetica.Module.Web/Controllers/WidhCustomerLoockupController.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Web.Editors.ASPx;
+using DevExpress.Web;
+using System.Web.UI.WebControls;
+
+namespace ActivacionProfetica.Module.Web.Controllers
+{
+    public class WidhCustomerLoockupController : ViewController<ListView>
+    {
+        public WidhCustomerLoockupController()
+        {
+            TargetViewId = "Customer_LookupListView";
+        }
+
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            ASPxGridListEditor gridListEditor = View.Editor as ASPxGridListEditor;
+            if (gridListEditor != null)
+            {
+                ASPxGridView gridView = gridListEditor.Grid;
+                gridView.Settings.UseFixedTableLayout = true;
+                gridView.Settings.HorizontalScrollBarMode = ScrollBarMode.Auto;
+                gridView.Width = Unit.Percentage(100);
+                foreach (WebColumnBase column in gridView.Columns)
+                {
+                    IColumnInfo columnInfo = ((IDataItemTemplateInfoProvider)gridListEditor).GetColumnInfo(column);
+                    if (columnInfo != null)
+                    {
+                        IModelColumn modelColumn = (IModelColumn)columnInfo.Model;
+                        column.Width = Unit.Pixel(modelColumn.Width);
+                    }
+                }
+            }
+        }
+    }
+}
--- ActivacionProfetica.Module.Web/Editors/APLookupPropertyEditor.cs
+++ ActivacionProfetica.Module.Web/Editors/APLookupPropertyEditor.cs
@@ -2,7 +2,9 @@
 using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.Web.Editors.ASPx;
+using DevExpress.Web;
 using System;
+using System.Web.UI.WebControls;
 
 namespace ActivacionProfetica.Module.Web.Editors
 {
@@ -12,6 +14,32 @@ namespace ActivacionProfetica.Module.Web.Editors
         public APLookupPropertyEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model)
         {
 
+        }
+
+        protected override void SetupControl(WebControl control)
+        {
+            base.SetupControl(control);
+            if (ViewEditMode == ViewEditMode.Edit)
+            {
+                ASPxGridLookup gridControl = control as ASPxGridLookup;
+
+                ASPxGridView gridView = gridControl.GridView;
+                gridView.Settings.UseFixedTableLayout = true;
+                gridView.Settings.HorizontalScrollBarMode = ScrollBarMode.Auto;
+                gridView.Width = Unit.Percentage(100);
+                foreach (WebColumnBase column in gridView.Columns)
+                {
+                    if (column.Caption == "CI")
+                    {
+                        column.Width = Unit.Percentage(20);
+                    }
+                    else
+                    {
+                        column.Width = Unit.Percentage(80);
+                    }
+
+                }
+            }
         }
     }
 }
